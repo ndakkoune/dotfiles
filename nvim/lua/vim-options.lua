79ceb5e28bc180defa2682a2f803bb0f043227df
Nabil Dakkoune <nabil.dakkoune.86@hotmail.com>
2024-09-13 09:27:48 +0200

    Install vim-tmux-vigator and add bindings,

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -5,3 +5,8 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.cmd("set number")
 vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
