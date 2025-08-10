2e6bc53fba6df768802eaa3474852c63a5a87c9b
Nabil Dakkoune <nabil.dakkoune.86@hotmail.com>
2025-08-10 07:59:04 +0200

    feat(nvim): add mini.icons plugin and update mini.comment

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,7 +1,16 @@
 return {
-  'echasnovski/mini.comment',
-  version = false,
-  config = function()
-    require('mini.comment').setup()
-  end
+  {
+    'echasnovski/mini.nvim',
+    version = '*',
+    config = function()
+      require('mini.icons').setup()
+    end
+  },
+  {
+    'echasnovski/mini.comment',
+    version = false,
+    config = function()
+      require('mini.comment').setup()
+    end
+  }
 }
