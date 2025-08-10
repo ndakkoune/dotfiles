bf0a99f8198640e89d42185b3f5fa6b66703532b
Nabil Dakkoune <nabil.dakkoune.86@hotmail.com>
2025-08-10 07:55:32 +0200

    perf(nvim): update neo-tree lazyloading

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,18 +1,44 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    opts = {
+      close_if_last_window = false,
+    },
+    config = function(_, opts)
+      require("neo-tree").setup(opts)
+      vim.keymap.set('n', '<C-n>', function()
+        if vim.fn.exists(':Neotree') == 2 then
+          vim.cmd('Neotree toggle filesystem reveal left')
+        else
+          vim.cmd('Neotree filesystem reveal left')
+        end
+      end, { noremap = true, silent = true })
+    end,
   },
-  config = function()
-    vim.keymap.set('n', '<C-n>', function()
-      if vim.fn.exists(':Neotree') == 2 then
-        vim.cmd('Neotree toggle filesystem reveal left')
-      else
-        vim.cmd('Neotree filesystem reveal left')
-      end
-    end, { noremap = true, silent = true })
-  end
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim", -- makes sure that this loads after Neo-tree.
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+  -- config = function()
+  --   vim.keymap.set('n', '<C-n>', function()
+  --     if vim.fn.exists(':Neotree') == 2 then
+  --       vim.cmd('Neotree toggle filesystem reveal left')
+  --     else
+  --       vim.cmd('Neotree filesystem reveal left')
+  --     end
+  --   end, { noremap = true, silent = true })
+  -- end
 }
