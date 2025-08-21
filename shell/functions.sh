654fcdad6f84a829459823c67c59d5240885f6dd
Nabil Dakkoune <nabil.dakkoune.86@hotmail.com>
2025-08-21 08:17:33 +0200

    feat(shell): add PATH-related functions

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,3 +1,17 @@
+path_remove() {
+    PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')
+}
+
+path_append() {
+    path_remove "$1"
+    PATH="${PATH:+"$PATH:"}$1"
+}
+
+path_prepend() {
+    path_remove "$1"
+    PATH="$1${PATH:+":$PATH"}"
+}
+
 here() {
     local loc
     if [ "$#" -eq 1 ]; then
